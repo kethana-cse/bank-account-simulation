625da6ebff98a4c057460af19291ccab112ccee6
kethana-cse <kethanarunkana@gmail.com>
2026-02-28 11:18:40 +0000

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,52 @@
+
+class BankAccount:
+    def __init__(self, name, balance=0):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print("Amount Deposited Successfully!")
+        else:
+            print("Invalid Amount!")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient Balance!")
+        elif amount <= 0:
+            print("Invalid Amount!")
+        else:
+            self.balance -= amount
+            print("Amount Withdrawn Successfully!")
+
+    def check_balance(self):
+         print("Current Balance:", self.balance)
+
+name = input("Enter Account Holder Name: ")
+account = BankAccount(name)
+while True:
+    print("\n1. Deposit")
+    print("2. Withdraw")
+    print("3. Check Balance")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        amount = float(input("Enter amount to deposit: "))
+        account.deposit(amount)
+
+    elif choice == 2:
+        amount = float(input("Enter amount to withdraw: "))
+        account.withdraw(amount)
+
+    elif choice == 3:
+        account.check_balance()
+
+    elif choice == 4:
+        print("Thank you for banking!")
+        break
+
+    else:
+        print("Invalid Choice!")
